99ed0cde6fdf80279f19f853f8a63625300ef4f6
Lucas Manrick <lucasmanrick.ipms@gmail.com>
2025-05-23 14:42:05 -0300

    :recycle: corrigindo pequenas partes do codigo

--- src/main/java/com/generation/loja_games/controller/UsuarioController.java
+++ src/main/java/com/generation/loja_games/controller/UsuarioController.java
@@ -1,70 +1,70 @@
-//package com.generation.loja_games.controller;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.PutMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestHeader;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.generation.loja_games.model.Usuario;
-//import com.generation.loja_games.model.UsuarioLogin;
-//import com.generation.loja_games.service.UsuarioService;
-//
-//import jakarta.validation.Valid;
-//
-//@RestController
-//@RequestMapping("/usuarios")
-//@CrossOrigin(origins = "*", allowedHeaders = "*")
-//public class UsuarioController {
-//	
-//	@Autowired
-//	private UsuarioService usuarioService;
-//	
-//	@GetMapping()
-//	public ResponseEntity<List<Usuario>> getAllUsers () {
-//		return ResponseEntity.ok(usuarioService.getAllUsers());
-//	}
-//	
-//	
-//	
-//	@PostMapping("/cadastrar")
-//	public ResponseEntity<Usuario> post (@Valid @RequestBody Usuario usuario) {
-//		return usuarioService.cadastrarUsuario(usuario)
-//				.map(response -> ResponseEntity.status(HttpStatus.CREATED).body(response))
-//				.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
-//				
-//	}
-//	
-//	
-//	
-//	@PutMapping("/atualizar")
-//	public ResponseEntity<Usuario> put (@Valid @RequestBody Usuario usuario, @RequestHeader("Authorization") String auth) {
-//		
-//		
-//		return usuarioService.atualizarUsuario(usuario)
-//				.map(response -> ResponseEntity.status(HttpStatus.OK).body(response))
-//				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-//				
-//	}
-//	
-//	
-//	@PostMapping("/logar")
-//	public ResponseEntity<UsuarioLogin> autenticar (@Valid @RequestBody Optional<UsuarioLogin> usuarioLogin) {
-//		return usuarioService.autenticarUsuario(usuarioLogin)
-//				.map(response -> ResponseEntity.status(HttpStatus.OK).body(response))
-//				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
-//				
-//	}
-//	
-//	
-//}
-//
+package com.generation.loja_games.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.loja_games.model.Usuario;
+import com.generation.loja_games.model.UsuarioLogin;
+import com.generation.loja_games.service.UsuarioService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@GetMapping()
+	public ResponseEntity<List<Usuario>> getAllUsers () {
+		return ResponseEntity.ok(usuarioService.getAllUsers());
+	}
+	
+	
+	
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> post (@Valid @RequestBody Usuario usuario) {
+		return usuarioService.cadastrarUsuario(usuario)
+				.map(response -> ResponseEntity.status(HttpStatus.CREATED).body(response))
+				.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+				
+	}
+	
+	
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> put (@Valid @RequestBody Usuario usuario, @RequestHeader("Authorization") String auth) {
+		
+		
+		return usuarioService.atualizarUsuario(usuario)
+				.map(response -> ResponseEntity.status(HttpStatus.OK).body(response))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+				
+	}
+	
+	
+	@PostMapping("/logar")
+	public ResponseEntity<UsuarioLogin> autenticar (@Valid @RequestBody Optional<UsuarioLogin> usuarioLogin) {
+		return usuarioService.autenticarUsuario(usuarioLogin)
+				.map(response -> ResponseEntity.status(HttpStatus.OK).body(response))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+				
+	}
+	
+	
+}
+
